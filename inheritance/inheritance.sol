e5b59e73ca13935ad77733426fb84bbb58c6090e
"Dylan Clarke" <"dylanclarke890@gmail.com">
2022-04-17 12:51:48 +0100

    added example of inheritance

--- inheritance/inheritance.sol
+++ inheritance/inheritance.sol
@@ -1,29 +1,31 @@
 pragma solidity 0.5.11;
 
 contract ERC20Token {
-    string name;
+    string public name;
     mapping(address => uint) public balances;
+
+    constructor(string memory _name) public {
+        name = _name;
+    }
 
     function mint() public {
         balances[tx.origin] ++;
     }
 }
 
-contract MakePayment {
-    address public token;
-    address payable wallet;
+contract InheritedToken is ERC20Token {
+    string public symbol;
+    address[] public owners;
+    uint256 ownerCount;
 
-    constructor(address payable _wallet, address _token) public {
-        wallet = _wallet;
-        token = _token;
+    constructor(string memory _name, string memory _symbol) ERC20Token(_name) public {
+        name = _name;
+        symbol = _symbol;
     }
 
-    function() external payable {
-        buyToken();
-    }
-
-    function buyToken() public payable {
-        ERC20Token(address(token)).mint();
-        wallet.transfer(msg.value);
+    function mint() public {
+        super.mint();
+        ownerCount++;
+        owners.push(tx.origin);
     }
 }
